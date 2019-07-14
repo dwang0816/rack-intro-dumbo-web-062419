b89d38544ca37f9bf8970a98d7f1ead0c8fe5e5f
Dennis Wang <dwang0816@gmail.com>
2019-07-14 16:37:44 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, My Name Is Dennis Wang"
+    resp.write "Hello, my name is Dennis Wang"
     resp.finish
   end
 
